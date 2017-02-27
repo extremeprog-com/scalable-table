0f785768f9654b73e84ca448c5800b7ade5de898
trean <trean.mi@gmail.com>
2017-02-27 23:51:24 +0300

    bigger example

--- example/js/controller.js
+++ example/js/controller.js
@@ -6,29 +6,48 @@ app.controller('MyController', ['$scope', '$http', '$rootScope', function ($scop
     // should return promise which returns array
 
     return new Promise(function (resolve) {
-      $scope.tableData = data.map(function (item) {
+      $scope.tableData = function () {
         var arr = [];
-        arr.push(item.name.first + " " + item.name.last);
-        arr.push(item.email);
-        arr.push(item.company);
+
+        data.forEach(function (obj) {
+          var subArr = [];
+
+          for (var key in obj) {
+            var value = obj[key];
+            subArr.push(value);
+          }
+          arr.push(subArr);
+        });
 
         return arr;
-      });
+      };
 
       var _to = setTimeout(function () {
         clearTimeout(_to);
-        resolve($scope.tableData);
+        resolve($scope.tableData());
       }, 800);
     });
   };
 
 
   $scope.dataColumns = ["lalala", "prprpr"];
-  // $scope.data = [[1,2],[3,4]];
+  $scope.columns = function () {
+    var cell = [];
+
+    for (var key in data[0]) {
+      var obj = {};
+      obj.Name = key;
+      obj.width = 200;
+      cell.push(obj);
+    }
+
+    return cell;
+  };
+
   $scope.no = 1;
 
   $scope.length = function () {
-    return 5;
+    return 1000;
   };
 
   $scope.gotoFoo = function (key) {
